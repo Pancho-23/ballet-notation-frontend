e13d539875d0b41d677851bb04bf45ca5559ccef
Francisco <seonihil@gmail.com>
2024-03-25 21:58:23 -0300

    Almost Key Navigation

--- src/components/BlockInput.js
+++ src/components/BlockInput.js
@@ -1094,36 +1094,36 @@ function BlockInput() {
       if (event.ctrlKey && event.key === "ArrowDown") {
         document.getElementById('next-step').click();
       }
-      //UNTIL HERE
       if (event.altKey && event.key === "2") {
         document.getElementById('divide-2').click();
       }
       if (event.altKey && event.key === "3") {
         document.getElementById('divide-3').click();
       }
-      if (event.altKey && event.key === "o") {
-        document.getElementById('open-class-button').click();
-      }
-      if (event.altKey && event.key === "s") {
-        document.getElementById('save-class-button').click();
-      }
-      if (event.altKey && event.key === "p") {
-        document.getElementById('open-step-button').click();
-      }
-      if (event.altKey && event.key === "v") {
-        document.getElementById('save-step-button').click();
-      }
-      if (event.altKey && event.key === "a") {
+      if (event.altKey && event.key === "ArrowRight") {
         document.getElementById('add-phrase-button').click();
       }
-      if (event.altKey && event.key === "d") {
+      if (event.altKey && event.key === "ArrowLeft") {
         document.getElementById('del-phrase-button').click();
       }
-      if (event.altKey && event.key === "+") {
+      if (event.altKey && event.key === "ArrowDown") {
         document.getElementById('add-step-button').click();
       }
-      if (event.altKey && event.key === "-") {
+      if (event.altKey && event.key === "ArrowUp") {
         document.getElementById('del-step-button').click();
+      }
+      //UNTIL HERE MUST CORRECT THE FOLLOWING ONES
+      if (event.key === " " && event.key === "o") {
+        document.getElementById('open-class-button').click();
+      }
+      if (event.key === " " && event.key === "s") {
+        document.getElementById('save-class-button').click();
+      }
+      if (event.key === " " && event.key === "p") {
+        document.getElementById('open-step-button').click();
+      }
+      if (event.key === " " && event.key === "v") {
+        document.getElementById('save-step-button').click();
       }
       if (event.shiftKey && event.key === "b") {
         document.getElementById('reset-block-button').click();
